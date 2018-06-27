fcd6c3dfb2a1e756c1472cc446a4d418b3f80d1d
hariririski <hariririski@gmail.com>
2018-06-27 15:18:01 +0700

    Final
    Final

--- application/views/admin/grafik_rs.php
+++ application/views/admin/grafik_rs.php
@@ -10,7 +10,7 @@ $tahun_max=max($data_tahun);?>
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
         <link href="<?php site_url(); ?>/asset/webroot/css/cake.generic.css" type="text/css" rel="stylesheet">
-        <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+        <script type="text/javascript" src="<?php echo site_url(); ?>data_umum/jquery.min.js"></script>
         <script type="text/javascript">
             $(document).ready(function() {
                 //default
